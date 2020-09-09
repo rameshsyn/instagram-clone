804bee32db1aed67d08a505e32d962875b4d84c7
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2020-09-09 10:21:37 +0545

    Add space

--- src/components/ScreenLayout.js
+++ src/components/ScreenLayout.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import BottomTab from './BottomTab';
+import {View, StyleSheet} from 'react-native';
 
 const ScreenLayout = ({children, navigation}) => {
   return (
-    <>
-      {children}
+    <View style={styles.layout}>
+      <View style={styles.screen}>{children}</View>
       <BottomTab navigation={navigation} />
-    </>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+  },
+  screen: {
+    padding: 20,
+  },
+});
 
 export default ScreenLayout;
